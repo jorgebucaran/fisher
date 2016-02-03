9542f38a0c6fca744484a4b3c37cfca9a088db62
Jorge Bucaran <mail@jorgebucaran.com>
2016-02-04 01:32:23 +0900

    Remove superfluous comment.

--- functions/__fisher_plugin_link.fish
+++ functions/__fisher_plugin_link.fish
@@ -1,7 +1,3 @@
 function __fisher_plugin_link -a options source target
-
-    # Why not simply run `ln args` inside __fisher_plugin_enable?
-    # We want to provide a hook for future plugins to override core functionality.
-
     ln $options $source $target
 end
