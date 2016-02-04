85bbcccd27ff9884acfb53b94b3b3048ca1a6954
Jorge Bucaran <mail@jorgebucaran.com>
2016-02-04 13:52:43 +0900

    Display deps as they install along with parent plugin.

--- functions/__fisher_deps_install.fish
+++ functions/__fisher_deps_install.fish
@@ -1,13 +1,7 @@
 function __fisher_deps_install -a path
-    printf 0
-
-    if test -s $path/bundle -o -s $path/fishfile
-        printf "Installing dependencies >> (%s)\n" (
-            for file in $path/{bundle,fishfile}
-                __fisher_list $file
-            end | paste -sd ' ' -
-            ) > /dev/stderr
-
-        cat $path/{bundle,fishfile} | fisher_install ^| sed -En 's/([0-9]+) plugin\/s.*/\1/p'
-    end
+    for file in $path/{fishfile,bundle}
+        if test -s $file
+            fisher_install < $file | sed -En 's/^.+([0-9]+) plugin\/s.*/\1/p'
+        end
+    end | awk '{ n = n + $0 } END { print n ? n : 0 }'
 end
