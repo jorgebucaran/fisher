1856524e74c2c252a034f7c13863f2c21681efba
Jorge Bucaran <mail@jorgebucaran.com>
2016-02-16 10:43:11 +0900

    Do not fail if Makefile is missing or empty w/ support for fish 2.1.2

--- functions/__fisher_path_make.fish
+++ functions/__fisher_path_make.fish
@@ -1,14 +1,16 @@
 function __fisher_path_make -a path
-    if test -s $path/Makefile -o -s $path/makefile
-        pushd $path
-
-        set -e argv[1]
-
-        if not make $argv
-            popd
-            return 1
-        end
-
-        popd
+    if test ! -s $path/Makefile -o ! -s $path/makefile
+        return
     end
+
+    pushd $path
+
+    set -e argv[1]
+
+    if not make $argv
+        popd
+        return 1
+    end
+
+    popd
 end
