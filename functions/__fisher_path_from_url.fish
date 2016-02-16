11f5b991a35daefb47e0ede47929bcad7d25ff5b
Jorge Bucaran <jbucaran@gmail.com>
2016-02-16 14:36:29 +0900

    Remove git -C to improve support in exotic systems.

--- functions/__fisher_path_from_url.fish
+++ functions/__fisher_path_from_url.fish
@@ -1,17 +1,15 @@
 function __fisher_path_from_url -a url
-
-    # What is the difference between path-from-url and url-from-path?
-
-    # Both functions use 'git ... --get-url'. The first one compares the given URL with
-    # the ls-remote of each repo in the cache and returns the path of the first match.
-    # The other one returns the ls-remote of the given path.
-
     for file in $fisher_cache/*
+        pushd $file
+
         switch "$url"
-            case (git -C $file ls-remote --get-url)
+            case (git ls-remote --get-url)
                 printf "%s\n" $file
+                popd
                 return
         end
+
+        popd
     end
 
     return 1
--- test/path-update.fish
+++ test/path-update.fish
@@ -1,18 +1,26 @@
+set -g path $DIRNAME/.t-$TESTNAME-(random)
+
 function -S setup
+    mkdir -p $path/foo
+
     function git
-        switch "$argv"
-            case "-C foo/bar checkout master --quiet"
-                printf checkout-
-            case "-C foo/bar pull --rebase origin master --quiet"
-                printf pull-rebase
+        if test (pwd) = $path/foo
+            switch "$argv"
+                case "checkout master --quiet"
+                    printf checkout-
+
+                case "pull --rebase origin master --quiet"
+                    printf pull-rebase
+            end
         end
     end
 end
 
 function -S teardown
     functions -e git
+    rm -rf $path
 end
 
 test "$TESTNAME - Use Git to update given path"
-    "checkout-pull-rebase" = (__fisher_path_update foo/bar)
+    "checkout-pull-rebase" = (__fisher_path_update $path/foo)
 end
