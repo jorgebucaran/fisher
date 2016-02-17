eba89acc27bf6cb8d46aa6540266caef11463adb
Jorge Bucaran <jbucaran@gmail.com>
2016-02-18 06:41:51 +0900

    Update function description.

--- functions/man.fish
+++ functions/man.fish
@@ -1,4 +1,4 @@
-function man -d "Format and display manual pages"
+function man -d "Format and display the on-line manual pages"
     set -lx MANPATH {$__fish_datadir,$fisher_config,$fisher_home}/man $MANPATH ""
     command man $argv
 end
