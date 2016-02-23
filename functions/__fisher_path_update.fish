aae66ae0d8fae67df310b2afe3ed38a65a11b5bf
Jorge Bucaran <mail@jorgebucaran.com>
2016-02-24 08:45:53 +0900

    Ignore git stash standard error.

--- functions/__fisher_path_update.fish
+++ functions/__fisher_path_update.fish
@@ -2,9 +2,9 @@ function __fisher_path_update -a path
     pushd $path
 
     git checkout master --quiet
-    git stash --quiet
+    git stash --quiet ^ /dev/null
     git pull --rebase origin master --quiet
-    git stash apply --quiet
+    git stash apply --quiet ^ /dev/null
 
     popd
 end
