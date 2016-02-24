2f325c38aee915955062d674b12f31c55c4bcae7
Jorge Bucaran <jbucaran@gmail.com>
2016-02-24 23:59:55 +0900

    Update is now true to its word :rage1:
    The update mechanism updates master only. If you are working in branch
    foo, the branch name is saved and restored later. First attempt a naive
    git pull --rebase origin master, and if that fails, abort the rebase,
    fetch origin master, reset to head and clean directory.

--- functions/__fisher_path_update.fish
+++ functions/__fisher_path_update.fish
@@ -1,10 +1,28 @@
 function __fisher_path_update -a path
     pushd $path
 
+    set -l branch
+
+    if not set branch (sed "s|.*/||" < .git/HEAD)
+        return 1
+    end
+
     git checkout master --quiet
+
     git stash --quiet ^ /dev/null
-    git pull --rebase origin master --quiet
+
+    if not git pull --rebase origin master --quiet ^ /dev/null
+        git rebase --abort
+        git fetch origin master --quiet
+        git reset --hard FETCH_HEAD --quiet
+        git clean -d --force --quiet
+    end
+
     git stash apply --quiet ^ /dev/null
+
+    if test ! -z "$branch"
+        git checkout "$branch" --quiet
+    end
 
     popd
 end
