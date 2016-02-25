34b880f6beed94dde88a308269460d9f434ffd81
Jorge Bucaran <jbucaran@gmail.com>
2016-02-25 10:08:48 +0900

    Make __fisher_key_bindings_undo smarter. Close #121.
    Until now key_bindings.fish only allowed simple bind <args>
    staments and would fail for conditional statements, etc.

--- functions/__fisher_key_bindings_disable.fish
+++ functions/__fisher_key_bindings_disable.fish
@@ -1,17 +1,11 @@
 function __fisher_key_bindings_disable -a plugin user_key_bindings
-    fish_indent < $fisher_binds \
-        | __fisher_key_bindings_undo $plugin \
-        | source
+    fish_indent < $fisher_binds | __fisher_key_bindings_undo $plugin | source ^ /dev/null
 
-    __fisher_key_bindings_delete $plugin \
-        > $fisher_binds.tmp \
-        < $fisher_binds
+    __fisher_key_bindings_delete $plugin > $fisher_binds.tmp < $fisher_binds
 
     command mv -f $fisher_binds.tmp $fisher_binds
 
     if test ! -s $fisher_binds
-        debug "Remove fisherman key bindings '%s'" "$user_key_bindings"
-
         sed -i.tmp '/__fisher_key_bindings/d' $user_key_bindings
         command rm -f $user_key_bindings.tmp
     end
--- functions/__fisher_key_bindings_undo.fish
+++ functions/__fisher_key_bindings_undo.fish
@@ -1,3 +1,3 @@
 function __fisher_key_bindings_undo -a plugin
-    sed -n "/##$plugin##/,/##$plugin##/{s/bind /bind -e /p;};"
+    sed -n "/##$plugin##/,/##$plugin##/{s/^ *bind /bind -e /p;};"
 end
