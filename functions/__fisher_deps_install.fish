dbd04352fa1eb90829c065aa3bbc93c1b8b8c54a
Jorge Bucaran <mail@jorgebucaran.com>
2016-03-01 18:19:00 +0900

    Remove debug from __fisher_deps_install.

--- functions/__fisher_deps_install.fish
+++ functions/__fisher_deps_install.fish
@@ -1,7 +1,6 @@
 function __fisher_deps_install -a path
     for file in $path/{fishfile,bundle}
         if test -s $file
-            debug "Install deps %s" "$file"
             fisher_install < $file | sed -En 's/^.+([0-9]+) plugin\/s.*/\1/p'
         end
     end | awk '{ n = n + $0 } END { print n ? n : 0 }'
