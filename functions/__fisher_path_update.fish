6989dc7c7fe4f4ffec3453d62ce339fb5783ef4d
Jorge Bucaran <jbucaran@gmail.com>
2016-03-02 10:39:13 +0900

    Fix regression causing update to fail in some plugins.

--- functions/__fisher_path_update.fish
+++ functions/__fisher_path_update.fish
@@ -7,22 +7,21 @@ function __fisher_path_update -a path
         return 1
     end
 
-    if test "$branch" != master
-        git stash --quiet ^ /dev/null
-        git checkout master --quiet ^ /dev/null
-    end
+    git stash --quiet ^ /dev/null
+    git checkout master --quiet ^ /dev/null
 
-    if not git pull --rebase origin master --quiet ^ /dev/null
+    if not git pull --rebase origin master --quiet
         git rebase --abort --quiet
         git fetch origin master --quiet
         git reset --hard FETCH_HEAD --quiet
         git clean -d --force --quiet
-    end
+    end ^ /dev/null
 
-    if test "$branch" != master
+    if test ! -z "$branch"
         git checkout "$branch" --quiet
-        git stash apply --quiet ^ /dev/null
     end
 
+    git stash apply --quiet ^ /dev/null
+    
     popd
 end
