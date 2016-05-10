3ae1b1897ef693f0677ed8bd2a746effa75ae23e
Jorge Bucaran <mail@jorgebucaran.com>
2016-05-10 19:26:35 +0900

    2.6.12

--- fisher.fish
+++ fisher.fish
@@ -15,7 +15,7 @@ function fisher
             return 1
     end
 
-    set -g fisher_version "2.6.11"
+    set -g fisher_version "2.6.12"
     set -g fisher_spinners ⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏
 
     function __fisher_show_spinner
