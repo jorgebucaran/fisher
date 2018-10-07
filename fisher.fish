jorgebucaran/fisher

commit 7850f78e8a978dde82e983dab5334aaf41c9cbff

feat: copy non fish files to $fisher_path; close #456

fisher.fish
@@ -286,7 +286,8 @@ end
 
 function _fisher_pkg_install -a pkg
     set -l name (echo $pkg | command sed "s|^.*/||")
-    for source in $pkg/{functions,completions,conf.d,}/*.fish
+    set -l files $pkg/{functions,completions,conf.d}/* $pkg/*.fish
+    for source in $files
         set -l target (echo "$source" | command sed 's|^.*/||')
         switch $source
             case $pkg/conf.d\*
@@ -305,35 +306,45 @@ function _fisher_pkg_install -a pkg
         end
         echo "linking $target" | command sed "s|$HOME|~|" >&2
         command ln -f $source $target
-        source $target >/dev/null 2>/dev/null
+        switch $target
+            case \*.fish
+                source $target >/dev/null 2>/dev/null
+        end
     end
 end
 
 function _fisher_pkg_uninstall -a pkg
     set -l name (echo $pkg | command sed "s|^.*/||")
-    for source in $pkg/{conf.d,completions,functions,}/*.fish
+    set -l files $pkg/{conf.d,completions,functions}/* $pkg/*.fish
+    for source in $files
         set -l target (echo "$source" | command sed 's|^.*/||')
         set -l filename (echo "$target" | command sed 's|.fish||')
         switch $source
             case $pkg/conf.d\*
-                emit {$filename}_uninstall
-                command rm -f $fisher_path/conf.d/$target
+                if test "$filename.fish" = "$target"
+                    emit "$filename"_uninstall
+                end
+                set target conf.d/$target
             case $pkg/completions\*
-                command rm -f $fisher_path/completions/$target
-                complete -ec $filename
+                if functions -q "$filename"
+                    complete -ec $filename
+                end
+                set target completions/$target
             case $pkg/{,functions}\*
                 switch $target
                     case uninstall.fish
                         source $source
                         continue
                     case init.fish key_bindings.fish
-                        set target $fisher_path/conf.d/$name\_$target
+                        set target conf.d/$name\_$target
                     case \*
-                        set target $fisher_path/functions/$target
+                        set target functions/$target
                 end
-                command rm -f $target
-                functions -e $filename
+                if functions -q "$filename"
+                    functions -e $filename
+                end
         end
+        command rm -f $fisher_path/$target
     end
     if not functions -q fish_prompt
         source "$__fish_datadir/functions/fish_prompt.fish"

commit 2a6c36b8a9812bfddf9d10923efcbc880d041090

fix: use string check to determine if filename is a fish file; fix #459
Bug was introduced in 7850f78e8a978dde82e983dab5334aaf41c9cbff.

When removing a file don't use `functions -q $filename` as a loose
check for a .fish file. Use a string match instead.

`functions -q $filename` will cause the function defined in the file
to be autoloaded (if it wasn't already) which can cause trouble with
some "dubious" packages that run arbitrary code in function files.

fisher.fish
@@ -324,16 +324,13 @@ function _fisher_pkg_uninstall -a pkg
         set -l filename (echo "$target" | command sed 's|.fish||')
         switch $source
             case $pkg/conf.d\*
-                if test "$filename.fish" = "$target"
-                    emit "$filename"_uninstall
-                end
+                test "$filename.fish" = "$target"; and emit "$filename"_uninstall
                 set target conf.d/$target
             case $pkg/completions\*
-                if functions -q "$filename"
-                    complete -ec $filename
-                end
+                test "$filename.fish" = "$target"; and complete -ec $filename
                 set target completions/$target
             case $pkg/{,functions}\*
+                test "$filename.fish" = "$target"; and functions -e $filename
                 switch $target
                     case uninstall.fish
                         source $source
@@ -342,9 +339,6 @@ function _fisher_pkg_uninstall -a pkg
                         set target conf.d/$name\_$target
                     case \*
                         set target functions/$target
-                end
-                if functions -q "$filename"
-                    functions -e $filename
                 end
         end
         command rm -f $fisher_path/$target