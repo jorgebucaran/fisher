1f080b04570250a62efff34821de7aa1fe9be676
Jorge Bucaran <mail@jorgebucaran.com>
2021-12-28 13:42:08 -0300

    Improve fisher -h (#691)

--- functions/fisher.fish
+++ functions/fisher.fish
@@ -15,6 +15,8 @@ function fisher --argument-names cmd --description "A plugin manager for Fish"
             echo "Options:"
             echo "       -v or --version  Print version"
             echo "       -h or --help     Print this help message"
+            echo "Variables:"
+            echo "       \$fisher_path  Plugin installation path. Default: ~/.config/fish"
         case ls list
             string match --entire --regex -- "$argv[2]" $_fisher_plugins
         case install update remove
