827daa79e03edf96b9fa8fdb234da1f9bd00cd9e
Alex Rutar <alex@rutar.org>
2022-02-02 18:43:35 +0000

    Improve fisher autocompletions
    Fix autocompletions when called like `fisher_path=/some/path fisher [TAB]`.

--- completions/fisher.fish
+++ completions/fisher.fish
@@ -1,7 +1,8 @@
+set --local fisher_subcommands install update remove list
 complete --command fisher --exclusive --long help --description "Print help"
 complete --command fisher --exclusive --long version --description "Print version"
-complete --command fisher --exclusive --condition __fish_use_subcommand --arguments install --description "Install plugins"
-complete --command fisher --exclusive --condition __fish_use_subcommand --arguments update --description "Update installed plugins"
-complete --command fisher --exclusive --condition __fish_use_subcommand --arguments remove --description "Remove installed plugins"
-complete --command fisher --exclusive --condition __fish_use_subcommand --arguments list --description "List installed plugins matching regex"
+complete --command fisher --exclusive --condition "not __fish_seen_subcommand_from $fisher_subcommands" --arguments install --description "Install plugins"
+complete --command fisher --exclusive --condition "not __fish_seen_subcommand_from $fisher_subcommands" --arguments update --description "Update installed plugins"
+complete --command fisher --exclusive --condition "not __fish_seen_subcommand_from $fisher_subcommands" --arguments remove --description "Remove installed plugins"
+complete --command fisher --exclusive --condition "not __fish_seen_subcommand_from $fisher_subcommands" --arguments list --description "List installed plugins matching regex"
 complete --command fisher --exclusive --condition "__fish_seen_subcommand_from update remove" --arguments "(fisher list)"
