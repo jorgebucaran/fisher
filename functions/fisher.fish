59228d6215cb4c36aff77008751736f540dc42d8
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-05 13:05:44 -0400

    Prepend `command` to curl and tar (#766)

--- functions/fisher.fish
+++ functions/fisher.fish
@@ -98,7 +98,7 @@ function fisher --argument-names cmd --description "A plugin manager for Fish"
 
                         echo Fetching (set_color --underline)\$url(set_color normal)
 
-                        if curl --silent -L \$url | tar -xzC \$temp -f - 2>/dev/null
+                        if command curl --silent -L \$url | command tar -xzC \$temp -f - 2>/dev/null
                             command cp -Rf \$temp/*/* $source
                         else
                             echo fisher: Invalid plugin name or host unavailable: \\\"$plugin\\\" >&2
